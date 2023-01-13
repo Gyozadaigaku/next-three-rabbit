a517199cc24d8684f6f39f51e365f14501bcb8dd
Gyozadaigaku <gaspardcube15@gmail.com>
2023-01-13 17:36:43 +0900

    ✨ add Rabbit component

---
+++ components/Rabbit/index.tsx
@@ -0,0 +1,14 @@
+import React, { useCallback, useEffect, useRef, useState } from "react";
+
+const Rabbit: React.FC = () => {
+  return (
+    <>
+      <h1>
+        🐰 <span>2023年 あけお</span> 🐰
+      </h1>
+      <footer>~~ Created by Jun ❤️ ~~</footer>
+    </>
+  );
+};
+
+export default Rabbit;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,18 @@ import type { NextPage } from "next";
 import dynamic from "next/dynamic";
 import Head from "next/head";
 
+const Rabbit = dynamic(() => import("../components/Rabbit"), {
+  ssr: false,
+  loading: () => <div>loading...</div>,
+});
+
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
         <title>🐰 2023年 あけお 🐰</title>
       </Head>
+      <Rabbit />
     </div>
   );
 };
